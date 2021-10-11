8d91b64d4504cdb14e48784a9fbe23651ea90630
tomoprogrammer <train.vegacat@gmail.com>
2021-10-11 13:10:13 +0000

    start

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_10_11_110835) do
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,5 +23,4 @@ ActiveRecord::Schema.define(version: 2021_10_11_110835) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
